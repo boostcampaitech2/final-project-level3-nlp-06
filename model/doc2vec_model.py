1d1782806f0ceba58b625ee16406084d78845cb9
baekTree <etknbe@gmail.com>
2021-12-20 11:08:07 +0000

    [MOD] Modified code of doc2vec
    Changed current directory to save model
    
    Changed function name

--- model/doc2vec_model.py
+++ model/doc2vec_model.py
@@ -3,10 +3,10 @@ import pandas as pd
 from konlpy.tag import Mecab
 from gensim.models.doc2vec import TaggedDocument
 from tqdm import tqdm
-
+import os
 import kss
 from input_preprocess import remove_email,  morph_filter
-
+from load_doc2vec import *
 
 # input이 doc
 def preprocess_news(doc):
@@ -25,24 +25,26 @@ def tokenize(text):
     mecab = Mecab() 
     return mecab.nouns(str(text))
 
-def main():
-    # doc = input()
-    # Example
-    doc = "삼성전자가 고성능 솔리드스테이트드라이브(SSD)와 그래픽 D램 등 첨단 메모리 반도체를 글로벌 자동차 제조사에 공급한다고 16일 밝혔다."
-    
-    if os.path.isfile("/opt/ml/final_project/data/dart/dart_fin.doc2vec") :
-        model = doc2vec.Doc2Vec.load('/opt/ml/final_project/data/dart/dart_fin.doc2vec') # 경로를 바꿔주세요
+
+def doc2vec_inference(doc):
+    if os.path.isfile("./dart_fin.doc2vec") :
+        model = doc2vec.Doc2Vec.load('./dart_fin.doc2vec') # 경로를 바꿔주세요
     else:
         model = load_doc_model()
     
     text = preprocess_news(doc)
     docs_mod = tokenize(text)
-
-    scriptV = model.infer_vector(docs_mod, alpha=0.025, min_alpha=0.025, epochs=50, steps=None)
-
+    scriptV = model.infer_vector(docs_mod, alpha=0.025, min_alpha=0.025, epochs=50)
     result = model.docvecs.most_similar(positive=[scriptV], topn=3)
     total_result = [corp[0] for corp in result]
-    print(total_result)
+    return total_result
+
+def main():
+    # doc = input()
+    # Example
+    doc = "삼성전자가 고성능 솔리드스테이트드라이브(SSD)와 그래픽 D램 등 첨단 메모리 반도체를 글로벌 자동차 제조사에 공급한다고 16일 밝혔다."
+    result = doc2vec_inference(doc)
+    print(result)
 
 if __name__ == '__main__':
     main()
