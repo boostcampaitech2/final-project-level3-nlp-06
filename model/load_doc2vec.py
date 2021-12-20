623630eeb5cda67de82b2f204a281146cdc6a68f
baekTree <etknbe@gmail.com>
2021-12-20 11:08:07 +0000

    [MOD] Modified code of doc2vec
    Changed current directory to save model
    
    Changed function name

--- model/load_doc2vec.py
+++ model/load_doc2vec.py
@@ -16,18 +16,19 @@ def load_doc_model():
         text = row['clean']
         tag = row['기업 이름']
         tagged_corpus_list.append(TaggedDocument(tags=[tag], words=mecab.morphs(text)))
-    
     model = doc2vec.Doc2Vec(vector_size=300, alpha=0.025, min_alpha=0.025, workers=8, window=8)
 
     # Vocabulary 빌드
     model.build_vocab(tagged_corpus_list)
-    print(f"Tag Size: {len(model.docvecs.doctags.keys())}", end=' / ')
+    # print(f"Tag Size: {len(model.docvecs.doctags.keys())}", end=' / ')
 
     # Doc2Vec 학습
     model.train(tagged_corpus_list, total_examples=model.corpus_count, epochs=50)
 
     # 모델 저장
-    model.save('dart_fin.doc2vec')
+    model.save('./dart_fin.doc2vec')
 
     return doc2vec.Doc2Vec.load('dart_fin.doc2vec')
 
+if __name__ == '__main__':
+    load_doc_model()
